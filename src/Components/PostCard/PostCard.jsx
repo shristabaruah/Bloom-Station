1338fbbfe7bc75c620a8298ba79e330f5ca60e67
Shrista Baruah <eternalshrista@gmail.com>
2022-07-14 14:40:35 +0530

    feat: like and dislike

--- src/Components/PostCard/PostCard.jsx
+++ src/Components/PostCard/PostCard.jsx
@@ -15,22 +15,24 @@ import {
   PopoverContent,
   PopoverTrigger,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { BsThreeDots, BsFillBookmarkFill } from "react-icons/bs";
 import { MdModeEdit, MdDelete } from "react-icons/md";
-import { AiFillLike } from "react-icons/ai";
+import { AiFillLike, AiOutlineLike } from "react-icons/ai";
 import { Comment } from "../Comments/Comments";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import { useState } from "react";
-import { deletePost } from "../../Redux/AsyncThunk/postThunk";
+import {
+  deletePost,
+  dislikePost,
+  likePost,
+} from "../../Redux/AsyncThunk/postThunk";
 import { EditPostModal } from "../../Components/EditPostModal/EditPostModal";
-import {  useDisclosure } from "@chakra-ui/react";
 
-
-const PostCard = ({ img, post  }) => {
+const PostCard = ({ img, post }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [editedPost, setEditedPost] = useState(null);
@@ -53,154 +55,176 @@ const PostCard = ({ img, post  }) => {
     }
   };
 
-  const EditedPostHandler = (post)=>{
-    setEditedPost(post)
-    onOpen()
-  }
+  const EditedPostHandler = (post) => {
+    setEditedPost(post);
+    onOpen();
+  };
 
+  const isLiked = post?.likes.likedBy.some(
+    (currUser) => currUser._id === user._id
+  );
+
+  const likeHandler = async (postId) => {
+    isLiked
+      ? await dispatch(dislikePost({ postId, token }))
+      : await dispatch(likePost({ postId, token }));
+  };
+
+  const likeMessage = () => {
+    if (post?.likes.likeCount === 1) {
+      return `${post?.likes.likeCount} like`;
+    }
+    {
+      return `${post?.likes.likeCount} likes`;
+    }
+  };
   return (
-    <>    <Flex
-      mt="5"
-      flexDir="column"
-      bgColor="grey.100"
-      p="8"
-      gap="2"
-      borderRadius="8"
-      position="relative"
-      maxW="32rem"
-    >
-      <Flex justifyContent="space-between">
-        <Flex
-          gap="1"
-          justifyContent="space-evenly"
-          cursor="pointer"
-          onClick={() => navigate(`/profile/${post.username}`)}
-        >
-          <Avatar
-            size="sm"
-            src={
-              post.username === user.username ? user.avatarURL : post.avatarURL
-            }
-          />
-          <Heading as="h3" size="md">
-            {post?.firstName} {post?.lastName}
-          </Heading>
-        </Flex>
-        {isCurrentUser && (
-          <Popover>
-            <PopoverTrigger>
-              <IconButton
-                icon={<BsThreeDots />}
-                bgColor="transparent"
-                borderRadius="50"
-                _hover={{ bgColor: "brand.50", borderRadius: "50" }}
-              />
-            </PopoverTrigger>
-            <PopoverContent
-              maxW="fit-content"
-              p="6"
-              _focus={{ borderColor: "transparent" }}
-              bgColor="grey.200"
-            >
-              <PopoverArrow />
-              <PopoverCloseButton />
-              <Button
-                leftIcon={<MdModeEdit />}
-                variant="ghost"
-                _focus={{ borderColor: "transparent" }}
-                onClick={()=>EditedPostHandler(post)}
-              >
-                Edit
-              </Button>
-              <Button
-                leftIcon={<MdDelete />}
-                variant="ghost"
-                _focus={{ borderColor: "transparent" }}
-                onClick={deletePostHandler}
-              >
-                Delete
-              </Button>
-            </PopoverContent>
-          </Popover>
-        )}
-      </Flex>
-
-      {/* PostContent */}
-      <Box>
-        <Text mb="0.5rem">{post?.content}</Text>
-        {img ? (
-          <Box>
-            <Image src={img} alt="img" h="300" w="full" objectFit="cover" />
-          </Box>
-        ) : null}
-      </Box>
-
-      {/* buttons */}
+    <>
+      {" "}
       <Flex
-        borderTop="1px"
-        borderTopColor="grey.200"
-        borderBottom="1px"
-        borderColor="grey.200"
-        justifyContent="space-between"
+        mt="5"
+        flexDir="column"
+        bgColor="grey.100"
+        p="8"
+        gap="2"
+        borderRadius="8"
+        position="relative"
+        maxW="32rem"
       >
-        <Box fontSize="1rem" color="brand.500">
+        <Flex justifyContent="space-between">
+          <Flex
+            gap="1"
+            justifyContent="space-evenly"
+            cursor="pointer"
+            onClick={() => navigate(`/profile/${post.username}`)}
+          >
+            <Avatar
+              size="sm"
+              src={
+                post.username === user.username
+                  ? user.avatarURL
+                  : post.avatarURL
+              }
+            />
+            <Heading as="h3" size="md">
+              {post?.firstName} {post?.lastName}
+            </Heading>
+          </Flex>
+          {isCurrentUser && (
+            <Popover>
+              <PopoverTrigger>
+                <IconButton
+                  icon={<BsThreeDots />}
+                  bgColor="transparent"
+                  borderRadius="50"
+                  _hover={{ bgColor: "brand.50", borderRadius: "50" }}
+                />
+              </PopoverTrigger>
+              <PopoverContent
+                maxW="fit-content"
+                p="6"
+                _focus={{ borderColor: "transparent" }}
+                bgColor="grey.200"
+              >
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <Button
+                  leftIcon={<MdModeEdit />}
+                  variant="ghost"
+                  _focus={{ borderColor: "transparent" }}
+                  onClick={() => EditedPostHandler(post)}
+                >
+                  Edit
+                </Button>
+                <Button
+                  leftIcon={<MdDelete />}
+                  variant="ghost"
+                  _focus={{ borderColor: "transparent" }}
+                  onClick={deletePostHandler}
+                >
+                  Delete
+                </Button>
+              </PopoverContent>
+            </Popover>
+          )}
+        </Flex>
+
+        {/* PostContent */}
+        <Box>
+          <Text mb="0.5rem">{post?.content}</Text>
+          {img ? (
+            <Box>
+              <Image src={img} alt="img" h="300" w="full" objectFit="cover" />
+            </Box>
+          ) : null}
+        </Box>
+
+        {/* buttons */}
+        <Flex
+          borderTop="1px"
+          borderTopColor="grey.200"
+          borderBottom="1px"
+          borderColor="grey.200"
+          justifyContent="space-between"
+        >
+          <Box fontSize="1rem" color="brand.500">
+            <IconButton
+              icon={isLiked ? <AiFillLike /> : <AiOutlineLike />}
+              _focus={{ borderColor: "transparent" }}
+              size="md"
+              fontSize="1.5rem"
+              borderRadius="50"
+              color={isLiked ? "" : "brand.300"}
+              onClick={() => likeHandler(post._id)}
+            />
+            {post?.likes.likeCount > 0 ? likeMessage() : null}
+          </Box>
           <IconButton
-            icon={<AiFillLike />}
+            icon={<BsFillBookmarkFill />}
             _focus={{ borderColor: "transparent" }}
             size="md"
             fontSize="1.5rem"
             borderRadius="50"
             color="brand.400"
           />
-          {post?.likes.likeCount ? post?.likes.likeCount : null}
-        </Box>
-        <IconButton
-          icon={<BsFillBookmarkFill />}
-          _focus={{ borderColor: "transparent" }}
-          size="md"
-          fontSize="1.5rem"
-          borderRadius="50"
-          color="brand.400"
+        </Flex>
+        <Flex gap="2">
+          <Avatar size="sm" />
+          <InputGroup>
+            <Input
+              variant="unstyled"
+              border="1px"
+              borderColor="grey.200"
+              borderRadius="50"
+              pr="4"
+              pl="4"
+            />
+            <InputRightElement>
+              <Button
+                variant="ghost"
+                _hover={{ bgColor: "transparent" }}
+                size="sm"
+                mr="7"
+              >
+                REPLY
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+        </Flex>
+        {post?.comments?.length > 0
+          ? post?.comments.map((comment) => (
+              <Comment key={comment._id} comment={comment} />
+            ))
+          : null}
+      </Flex>
+      {isOpen ? (
+        <EditPostModal
+          isOpen={isOpen}
+          onClose={onClose}
+          editedPost={editedPost}
+          setEditedPost={setEditedPost}
         />
-      </Flex>
-      <Flex gap="2">
-        <Avatar size="sm" />
-        <InputGroup>
-          <Input
-            variant="unstyled"
-            border="1px"
-            borderColor="grey.200"
-            borderRadius="50"
-            pr="4"
-            pl="4"
-          />
-          <InputRightElement>
-            <Button
-              variant="ghost"
-              _hover={{ bgColor: "transparent" }}
-              size="sm"
-              mr="7"
-            >
-              REPLY
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </Flex>
-      {post?.comments?.length > 0
-        ? post?.comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
-          ))
-        : null}
-    </Flex>
-    {isOpen ? (
-          <EditPostModal
-            isOpen={isOpen}
-            onClose={onClose}
-            editedPost={editedPost}
-            setEditedPost={setEditedPost}
-            
-          />
-        ) : null}
+      ) : null}
     </>
   );
 };
--- src/Redux/AsyncThunk/index.js
+++ src/Redux/AsyncThunk/index.js
@@ -1,3 +1,10 @@
 export { login, signup, edit } from "./authThunk";
-export { getPost , addPost, deletePost ,editPost} from "./postThunk";
-export { getUsers , followUser , unfollowUser } from "./usersThunk";
+export {
+  getPost,
+  addPost,
+  deletePost,
+  editPost,
+  likePost,
+  dislikePost,
+} from "./postThunk";
+export { getUsers, followUser, unfollowUser } from "./usersThunk";
